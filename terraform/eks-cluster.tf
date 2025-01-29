1d7ee6bb0ff8dad5a0f70a160a071d7ee35de055
mickeysh <mickey.shaul@gmail.com>
2025-01-29 12:13:03 +0200

    update eks version

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.5.0"
+  version         = "20.33.1"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
